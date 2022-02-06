671d56f3e8890b6b83e4199592a386706e934097
Rodrigo Formigon <rodris.hud@gmail.com>
2022-02-06 10:52:50 -0300

    Update ex_for_1.py

--- exercicios/ex_for_1.py
+++ exercicios/ex_for_1.py
@@ -53,4 +53,3 @@ for i, produto in enumerate(produtos):
     if vendas2020[i] > vendas2019[i]:
         crescimento = vendas2020[i] / vendas2019[i] - 1
         print(f'{produto} vendeu R$ {vendas2019[i]:,} em 2019, R$ {vendas2020[i]:,} em 2020 e teve {crescimento:.1%} de crescimento.')
-    
